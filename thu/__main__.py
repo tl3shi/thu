2880f712a0ddce91df6d0d7f2cd3a44f7a9a722b
tanglei <mac@tanglei.name>
2013-11-23 23:15:15 +0800

    return exact infomation when login failed

--- thu/__main__.py
+++ thu/__main__.py
@@ -1,11 +1,31 @@
 import sys
 
+def print_usage():
+    print ('''usage:
+thu net
+thu net login
+thu net logout
+
+thu usereg iplist
+thu usereg ipup x.x.x.x
+thu usereg ipdown x.x.x.x
+thu usereg ipdown 0/1/2
+    ''')
+
 if len(sys.argv) < 2:
-    print("usage: thu module [command]")
+    print_usage();
     exit(0)
 
 module = sys.argv[1]
 command = sys.argv[2] if len(sys.argv) > 2 else "main"
 
+if (module not in ["net", "usereg"]):
+    print("no module: " + module)
+    print_usage();
+    exit(0)
+
 m = __import__(module, globals(), locals(), level = 1)
-getattr(m, command)(*sys.argv[3:])
+try:
+    getattr(m, command)(*sys.argv[3:])
+except AttributeError:
+    print('no method: ' + sys.argv[2])
--- thu/net.py
+++ thu/net.py
@@ -9,6 +9,27 @@ from .user import username, password
 
 NetUsage = namedtuple('NetUsage', 'id user traffic unknown timelen')
 
+response_code_map = {"user_tab_error": "认证程序未启动",
+"user_group_error": "您的计费组信息不正确",
+"non_auth_error": "您无须认证，可直接上网",
+"status_error": "用户已欠费，请尽快充值。",
+"available_error": "您的帐号已停用",
+"delete_error": "您的帐号已删除",
+"ip_exist_error": "IP已存在，请稍后再试。",
+"usernum_error": "用户数已达上限",
+"online_num_error": "该帐号的登录人数已超过限额\n" +
+    "请登录https://usereg.tsinghua.edu.cn断开不用的连接。",
+"mode_error": "系统已禁止WEB方式登录，请使用客户端",
+"time_policy_error": "当前时段不允许连接",
+"flux_error": "您的流量已超支",
+"minutes_error": "您的时长已超支",
+"ip_error": "您的 IP 地址不合法",
+"mac_error": "您的 MAC 地址不合法",
+"sync_error": "您的资料已修改，正在等待同步，请 2 分钟后再试。",
+"ip_alloc": "您不是这个地址的合法拥有者，IP 地址已经分配给其它用户。",
+"ip_invaild": "您是区内地址，无法使用。"
+        }
+
 def check():
     req = Request('http://net.tsinghua.edu.cn/cgi-bin/do_login', b'action=check_online')
     resp = urlopen(req).read().decode()
@@ -25,7 +46,9 @@ def login():
         })
     req = Request('http://net.tsinghua.edu.cn/cgi-bin/do_login', data.encode())
     resp = urlopen(req).read().decode()
-    print(resp)
+    if(resp in response_code_map.keys()):
+        print(response_code_map[resp])
+        exit(0)
     check()
 
 def logout():
